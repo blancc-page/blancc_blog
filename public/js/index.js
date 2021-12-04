eb9f5611fed561df9c47039d1ec5c9e84c46e7a1
blancc <blancc.666@gmail.com>
2021-12-04 12:12:47 +0300

    Hope this works

--- public/js/index.js
+++ public/js/index.js
@@ -276,7 +276,14 @@ app.post("/deleteft", function(req,res){
     }
   })
 });
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+
+if (port === null || port == ""){
+  port == 3000;
+}
+
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
   
